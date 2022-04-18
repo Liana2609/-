9bc4a796b3b86cce535a06a435e3113a16fc8d9f
petr26a@gmail.com <petr26a@gmail.com>
2022-04-18 15:14:40 +0300

    Добавьте файлы проекта.

---
+++ Название/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Название
+{
+    public class Class1
+    {
+        public static Model1 db = new Model1();
+    }
+}
